55534058bf7ee4377333a54d7da39b9e9e31820d
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2017-11-03 15:59:38 -0400

    Removed

--- admin/class-cortex-admin.php
+++ admin/class-cortex-admin.php
@@ -379,7 +379,6 @@ class Cortex_Admin {
 				<meta charset="utf-8">
 				<meta http-equiv="x-ua-compatible" content="ie=edge">
 				<meta name="viewport" content="width=device-width, initial-scale=1">
-				<link href="https://fonts.googleapis.com/css?family=Roboto:400,700|Roboto+Condensed:700" rel="stylesheet">
 				<?php wp_head() ?>
 			</head>
 			<body <?php body_class('preview') ?>>
